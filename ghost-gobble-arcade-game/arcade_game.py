a30e3a9fb86978126fc5a203f1b3175d4a989f9e
Khashayar Nourian <knourianchali@gmail.com>
2023-04-08 12:10:12 +0330

    Ghost Gobble Arcade Game solution

--- ghost-gobble-arcade-game/arcade_game.py
+++ ghost-gobble-arcade-game/arcade_game.py
@@ -8,8 +8,7 @@ def eat_ghost(power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - can the ghost be eaten?
     """
-
-    pass
+    return power_pellet_active and touching_ghost
 
 
 def score(touching_power_pellet, touching_dot):
@@ -20,7 +19,7 @@ def score(touching_power_pellet, touching_dot):
     :return: bool - has the player scored or not?
     """
 
-    pass
+    return touching_power_pellet or touching_dot
 
 
 def lose(power_pellet_active, touching_ghost):
@@ -31,7 +30,7 @@ def lose(power_pellet_active, touching_ghost):
     :return: bool - has the player lost the game?
     """
 
-    pass
+    return touching_ghost and not power_pellet_active
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -43,4 +42,4 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :return: bool - has the player won the game?
     """
 
-    pass
+    return has_eaten_all_dots and not lose(power_pellet_active,touching_ghost)
