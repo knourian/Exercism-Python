306acda0158c34e426561cc530f3498ffbc5d0d1
Khashayar Nourian <knourianchali@gmail.com>
2023-04-08 10:04:39 +0330

    Guido's Gorgeous Lasagna Solution

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -7,12 +7,10 @@ This is a module docstring, used to describe the functionality
 of a module and its functions and/or classes.
 """
 
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
 
-#TODO: define the 'EXPECTED_BAKE_TIME' constant.
-
-
-#TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
-def bake_time_remaining():
+def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int - baking time already elapsed.
@@ -22,14 +20,23 @@ def bake_time_remaining():
     an argument and returns how many minutes the lasagna still needs to bake
     based on the `EXPECTED_BAKE_TIME`.
     """
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
+    
 
-    pass
+def preparation_time_in_minutes(number_of_layers):
+    """Returns how many minutes you would spend making Layers of lasagna
+    
+    :param number_of_layers: int - number of layers added to the lasagna.
+    
+    :return: int - minutes you would spend making Layers of lasagna
+    """
+    return PREPARATION_TIME * number_of_layers
 
-
-#TODO: Define the 'preparation_time_in_minutes()' function below.
-# You might also consider using 'PREPARATION_TIME' here, if you have it defined.
-
-
-
-#TODO: define the 'elapsed_time_in_minutes()' function below.
-# Remember to add a docstring (you can copy and then alter the one from bake_time_remaining.)
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Return the total number of minutes you've been cooking
+    
+    :param number_of_layers: int - number of layers added to the lasagna.
+    :param elapsed_bake_time: int - number of minutes the lasagna has been baking in the oven.
+    :return: int - the total number of minutes you've been cooking
+    """
+    return elapsed_bake_time + preparation_time_in_minutes(number_of_layers)
