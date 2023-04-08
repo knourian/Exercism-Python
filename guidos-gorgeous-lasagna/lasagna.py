21e297f64ae2b823b5355cda5e17fe9e41ce50fd
Khashayar Nourian <knourianchali@gmail.com>
2023-04-08 09:29:46 +0330

    Guido's Gorgeous Lasagna init

---
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -0,0 +1,35 @@
+"""Functions used in preparing Guido's gorgeous lasagna.
+
+Learn about Guido, the creator of the Python language:
+https://en.wikipedia.org/wiki/Guido_van_Rossum
+
+This is a module docstring, used to describe the functionality
+of a module and its functions and/or classes.
+"""
+
+
+#TODO: define the 'EXPECTED_BAKE_TIME' constant.
+
+
+#TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
+def bake_time_remaining():
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+
+    pass
+
+
+#TODO: Define the 'preparation_time_in_minutes()' function below.
+# You might also consider using 'PREPARATION_TIME' here, if you have it defined.
+
+
+
+#TODO: define the 'elapsed_time_in_minutes()' function below.
+# Remember to add a docstring (you can copy and then alter the one from bake_time_remaining.)
